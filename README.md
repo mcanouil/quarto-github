188861a520359f61b18ef5cbb10f7d8133dbfde2
Mickaël Canouil <8896044+mcanouil@users.noreply.github.com>
2024-07-14 14:20:54 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Github Extension For Quarto
+
+`github` is an extension for [Quarto](https://quarto.org) to automatically shortens and converts [GitHub references](https://docs.github.com/en/get-started/writing-on-github/working-with-advanced-formatting/autolinked-references-and-urls) into links.
+
+## Installing
+
+```bash
+quarto add mcanouil/quarto-github
+```
+
+This will install the extension under the `_extensions` subdirectory.
+If you're using version control, you will want to check in this directory.
+
+## Using
+
+Add the following to your YAML header:
+
+```yaml
+filters:
+  - github
+```
+
+Some references require a default repository to be set via the `repository-name` YAML key.
+
+```yml
+repository-name: jlord/sheetsee.js
+```
+
+## References
+
+Source: [Autolinked references and URLs - GitHub Docs](https://docs.github.com/en/get-started/writing-on-github/working-with-advanced-formatting/autolinked-references-and-urls).
+
+### Issues and pull requests
+
+| Reference type                                                                             | Raw reference                                    | Short link                      |
+|--------------------------------------------------------------------------------------------|--------------------------------------------------|---------------------------------|
+| Issue, discussion, or pull request URL (***`repository-name` is optional!***)              | `https://github.com/jlord/sheetsee.js/issues/26` | `#26` or `lord/sheetsee.js#26`  |
+| `#` and issue, discussion, or pull request number (***`repository-name` is required!***)   | `#26`                                            | `#26`                           |
+| `GH-` and issue, discussion, or pull request number (***`repository-name` is required!***) | `GH-26`                                          | `GH-26`                         |
+| `Username/Repository#` and issue, discussion, or pull request number                       | `jlord/sheetsee.js#26`                           | `jlord/sheetsee.js#26`          |
+| `Organization_name/Repository#` and issue, discussion, or pull request number              | `github-linguist/linguist#4039`                  | `github-linguist/linguist#4039` |
+
+## Commit SHAs
+
+| Reference type                                    | Raw reference                                                                          | Short link                               |
+|---------------------------------------------------|----------------------------------------------------------------------------------------|------------------------------------------|
+| Commit URL (***`repository-name` is optional!***) | `https://github.com/jlord/sheetsee.js/commit/a5c3785ed8d6a35868bc169f07e40e889087fd2e` | `a5c3785` or `jlord/sheetsee.js@a5c3785` |
+| SHA (***`repository-name` is required!***)        | `a5c3785ed8d6a35868bc169f07e40e889087fd2e`                                             | `a5c3785`                                |
+| `User@SHA` (***Not supported!*** )                | `jlord@a5c3785ed8d6a35868bc169f07e40e889087fd2e`                                       | ***Not supported!***                     |
+| `Username/Repository@SHA`                         | `jlord/sheetsee.js@a5c3785ed8d6a35868bc169f07e40e889087fd2e`                           | `jlord/sheetsee.js@a5c3785`              |
+
+## Example
+
+Here is the source code for a minimal example: [example.qmd](example.qmd).
---
+++ _extensions/github/github.lua
@@ -0,0 +1,138 @@
+--[[
+# MIT License
+#
+# Copyright (c) Mickaël Canouil
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+]]
+
+local function is_empty(s)
+  return s == nil or s == ''
+end
+
+local github_repository = nil
+
+function get_repository(meta)
+  local meta_github_repository = nil
+  if not is_empty(meta['repository-name']) then
+    meta_github_repository = pandoc.utils.stringify(meta['repository-name'])
+  end
+  github_repository = meta_github_repository
+  return meta
+end
+
+function issues(elem)
+  local user_repo = nil
+  local issue_number = nil
+  local type = nil
+  local short_link = nil
+
+  if elem.text:match("^#(%d+)$") then
+    issue_number = elem.text:match("^#(%d+)$")
+    user_repo = github_repository
+    type = "issues"
+    short_link = "#" .. issue_number
+  elseif elem.text:match("^([^/]+/[^/#]+)#(%d+)$") then
+    user_repo, issue_number = elem.text:match("^([^/]+/[^/#]+)#(%d+)$")
+    type = "issues"
+    short_link = user_repo .. "#" .. issue_number
+  elseif elem.text:match("^GH%-(%d+)$") then
+    issue_number = elem.text:match("^GH%-(%d+)$")
+    user_repo = github_repository
+    type = "issues"
+    short_link = "#" .. issue_number
+  elseif elem.text:match("^https://github.com/([^/]+/[^/]+)/([^/]+)/(%d+)$") then
+    user_repo, type, issue_number = elem.text:match("^https://github.com/([^/]+/[^/]+)/([^/]+)/(%d+)$")
+    if user_repo == github_repository then
+      short_link = "#" .. issue_number
+    else
+      short_link = user_repo .. "#" .. issue_number
+    end
+  end
+
+  local uri = nil
+  local text = nil
+  if not is_empty(short_link) and not is_empty(issue_number) and not is_empty(user_repo) and not is_empty(type) then
+    if type == "issues" or type == "discussions" or type == "pull" then
+      uri = "https://github.com/" .. user_repo .. '/' .. type .. '/' .. issue_number
+      text = pandoc.utils.stringify(short_link)
+    end
+  end
+
+  return uri, text
+end
+
+function commits(elem)
+  local user_repo = nil
+  local commit_sha = nil
+  local type = nil
+  local short_link = nil
+
+  if elem.text:match("^(%w+)$") and elem.text:len() == 40 then
+    commit_sha = elem.text:match("^(%w+)$")
+    user_repo = github_repository
+    type = "commit"
+    short_link = commit_sha:sub(1, 7)
+  elseif elem.text:match("^([^/]+/[^/@]+)@(%w+)$") then
+    user_repo, commit_sha = elem.text:match("^([^/]+/[^/@]+)@(%w+)$")
+    type = "commit"
+    short_link = user_repo .. "@" .. commit_sha:sub(1, 7)
+  elseif elem.text:match("^(%w+)@(%w+)$") then
+    user_repo, commit_sha = elem.text:match("^(%w+)@(%w+)$")
+    if commit_sha:len() == 40 then
+      type = "commit"
+      short_link = user_repo .. "@" .. commit_sha:sub(1, 7)
+    end
+  elseif elem.text:match("^https://github.com/([^/]+/[^/]+)/([^/]+)/(%w+)$") then
+    user_repo, type, commit_sha = elem.text:match("^https://github.com/([^/]+/[^/]+)/([^/]+)/(%w+)$")
+    if user_repo == github_repository then
+      short_link = commit_sha:sub(1, 7)
+    else
+      short_link = user_repo .. "@" .. commit_sha:sub(1, 7)
+    end
+  end
+
+  local uri = nil
+  local text = nil
+  if not is_empty(short_link) and not is_empty(commit_sha) and not is_empty(user_repo) and not is_empty(type) then
+    if type == "commit" and commit_sha:len() == 40 then
+      uri = "https://github.com/" .. user_repo .. '/' .. type .. '/' .. commit_sha
+      text = pandoc.utils.stringify(short_link)
+    end
+  end
+
+  return uri, text
+end
+
+function github(elem)
+  uri, text = issues(elem)
+  if not is_empty(uri) and not is_empty(text) then
+    return pandoc.Link(text, uri)
+  end
+
+  uri, text = commits(elem)
+  if not is_empty(uri) and not is_empty(text) then
+    return pandoc.Link(text, uri)
+  end
+end
+
+return {
+  {Meta = get_repository},
+  {Str = github}
+}
